50551fd40dffb6fa9dfc2c2afddd4aee2a022990
curry0 <62909270+curry0@users.noreply.github.com>
2022-10-13 14:12:09 +0200

    Added the backend

---
+++ AddressBook.API/AddressBook.API/Data/AddressBookDbContext.cs
@@ -0,0 +1,17 @@
+﻿using AddressBook.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBook.API.Data
+{
+    public class AddressBookDbContext : DbContext
+    {
+        public AddressBookDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //To access the table and communicate with that
+        public DbSet<Contact> Contacts { get; set; }
+
+        
+    }
+}
